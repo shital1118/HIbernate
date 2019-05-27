80714431120974327c6d2cc9ede124044f71fdab
shital1118 <50825320+shital1118@users.noreply.github.com>
2019-05-28 00:21:25 +0530

    Add files via upload

---
+++ Hibernate_Inheritance_ST/src/com/cdac/hibernate/model/OnlineCustomer.java
@@ -0,0 +1,35 @@
+package com.cdac.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+@Entity
+public class OnlineCustomer extends Customer{
+	
+	@Column(name="EMAIL")
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public OnlineCustomer() {
+		super();
+	}
+
+	public OnlineCustomer(String email) {
+		super();
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "OnlineCustomer [email=" + email + "]";
+	}
+	
+	
+
+}
